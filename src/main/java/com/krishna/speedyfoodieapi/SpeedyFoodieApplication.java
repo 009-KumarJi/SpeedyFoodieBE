a708e44719315735ac8ecc69e0cac3754c5e0430
Krishna Kumar <krishnakumar091@yahoo.com>
2025-04-08 11:30:00 +0530

    Stop tracking application.properties

---
+++ src/main/java/com/krishna/speedyfoodieapi/SpeedyFoodieApplication.java
@@ -0,0 +1,13 @@
+package com.krishna.speedyfoodieapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedyFoodieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedyFoodieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krishna/speedyfoodieapi/SpeedyFoodieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishna.speedyfoodieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedyFoodieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
